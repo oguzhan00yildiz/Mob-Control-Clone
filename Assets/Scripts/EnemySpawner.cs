ddaa224fd62c0924be82a8d66a2c1d39a7744588
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2023-05-02 17:18:19 +0300

    Enemy spawn position randomized

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,44 +6,53 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemySmallPrefab;
     [SerializeField] private float shootSpeed;
-    [SerializeField] private float nextFire;
     [SerializeField] private float fireRate;
+    [SerializeField] private float fireDuration;
+    private float nextFire = 0f; 
     private Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
+    private bool isSpawnDone;
+
+    void Update()
     {
+        
+        if (isSpawnDone==false)
+        {
+            if (Time.time > nextFire)
+            {
+                SpawnEnemy();
+                nextFire = Time.time + fireRate; 
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        Timer();
-    }
-
-    private void Timer()
-    {
-        if (Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate; 
-            SpawnEnemy();
-        }
+        StartCoroutine(Timer());
     }
 
     private void SpawnEnemy()
     {
-        GameObject enemySmall= Instantiate(enemySmallPrefab, transform.position, Quaternion.Euler(0,180,0)); 
-        rb=enemySmall.GetComponent<Rigidbody>();
-        StartCoroutine(ApplyForce());  
-
+        int rnd=Random.Range(-5,5);
+        GameObject enemySmall = Instantiate(enemySmallPrefab, transform.position+new Vector3(rnd,0,0), Quaternion.Euler(0, 180, 0));
+        rb = enemySmall.GetComponent<Rigidbody>();
+        StartCoroutine(ApplyForce());
     }
 
     IEnumerator ApplyForce()
     {
-        
         rb.AddForce(-transform.forward * shootSpeed, ForceMode.Impulse);
         yield return new WaitForSeconds(.4f);
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
     }
+
+    private IEnumerator Timer ()
+    {
+      yield return new WaitForSecondsRealtime(fireDuration);
+      isSpawnDone=true;
+
+    }
+
+    
 }
