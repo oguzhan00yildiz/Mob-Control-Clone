defb711e5b6b99a35dd50d8b0918a9c06a12d352
Mansur Besleney <besleneymansur@gmail.com>
2023-05-03 17:10:23 +0300

    Muzzle added to cannon

--- Assets/Scripts/CannonManager.cs
+++ Assets/Scripts/CannonManager.cs
@@ -11,6 +11,7 @@ public class CannonManager : MonoBehaviour
     [SerializeField] private float speed=5f; 
     [SerializeField]private Rigidbody rb;
     [SerializeField] private Slider bigPlayerSlider;
+    [SerializeField] private GameObject muzzle;
     private int chargeCount=0;
     public float shotSpeed=10f; 
     private float nextFire = 0.0f; 
@@ -23,7 +24,6 @@ public class CannonManager : MonoBehaviour
             Shoot();
         }
         else if(Input.GetMouseButtonUp(0) && chargeCount == 25) ShootBig();
-        Debug.Log(chargeCount);
         bigPlayerSlider.value = chargeCount;
     }
 
@@ -39,17 +39,17 @@ public class CannonManager : MonoBehaviour
 
     private void Shoot()
     {
-        GameObject cloneBlue= Instantiate(bluePrefab, transform.position, Quaternion.identity);
+        GameObject cloneBlue= Instantiate(bluePrefab, muzzle.transform.position, Quaternion.identity);
         rb=cloneBlue.GetComponent<Rigidbody>();
-        StartCoroutine(ApplyForce(shotSpeed));
+        StartCoroutine(ApplyForce());
         if(chargeCount <25) chargeCount++; 
     }
 
     private void ShootBig()
     {
-        GameObject BigPlayer= Instantiate(BigPlayerPrefab, transform.position, Quaternion.identity); 
+        GameObject BigPlayer= Instantiate(BigPlayerPrefab, muzzle.transform.position, Quaternion.identity); 
         rb=BigPlayer.GetComponent<Rigidbody>();
-        StartCoroutine(ApplyForce((shotSpeed*5)));  
+        StartCoroutine(ApplyForce());  
         chargeCount=0;
     }
     private void MoveHorizontal()
@@ -60,9 +60,9 @@ public class CannonManager : MonoBehaviour
         transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
     }
 
-    IEnumerator ApplyForce(float shotSpeed)
+    IEnumerator ApplyForce()
     {
-        rb.AddForce(transform.forward * shotSpeed, ForceMode.Impulse);
+        rb.AddForce(muzzle.transform.forward * shotSpeed, ForceMode.Impulse);
         yield return new WaitForSeconds(.4f);
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
